b831b337c0093d0f21763084d52f8fb30277704c
AnneVillasante <108015701+AnneVillasante@users.noreply.github.com>
2025-10-16 09:42:59 -0500

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
+const mysql = require('mysql2');
 
+// Crear la conexión
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Sapphire_27',
+  database: 'LunariaThreadsDB'
+});
+
+// Verificar la conexión
+connection.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos: ' + err.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos con ID ' + connection.threadId);
+});
+
+module.exports = connection;
