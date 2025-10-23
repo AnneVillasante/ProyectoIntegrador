18222bdd752617c6a9c9436e9347a17086301501
AnneVillasante <108015701+AnneVillasante@users.noreply.github.com>
2025-10-23 17:47:27 -0500

    update for correct run of app.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+require('dotenv').config({ debug: true });
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || '127.0.0.1',
