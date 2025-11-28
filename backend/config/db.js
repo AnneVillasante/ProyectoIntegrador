1a651972aec5533119e60469dbddac227c38f99c
AnneVillasante <108015701+AnneVillasante@users.noreply.github.com>
2025-11-28 17:23:44 -0500

    actualizar seguridad

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || '127.0.0.1',
-  port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Sapphire_27',
-  database: process.env.MYSQL_DATABASE || 'LunariaThreadsDB',
+  host: process.env.MYSQL_HOST || '127.0.0.1', // Valor por defecto para desarrollo
+  port: Number(process.env.MYSQL_PORT || '3306'), // Simplificado y con valor por defecto
+  user: process.env.MYSQL_USER || 'root', // Valor por defecto para desarrollo
+  password: process.env.MYSQL_PASSWORD, // ¡Importante! No tener contraseñas por defecto en el código
+  database: process.env.MYSQL_DATABASE || 'LunariaThreadsDB', // Valor por defecto para desarrollo
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
