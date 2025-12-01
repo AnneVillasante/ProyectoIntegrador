37c229c292efdeb95ef31740d315188aa6b73e0c
AnneVillasante <108015701+AnneVillasante@users.noreply.github.com>
2025-12-01 11:12:06 -0500

    corregir errores

--- backend/routes/subcategoriaRoutes.js
+++ backend/routes/subcategoriaRoutes.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const subcategoriaController = require('../controllers/subcategoriaController');
-const upload = require('../middleware/multerConfig'); // Importar configuración de Multer
+const multer = require('multer');
+const path = require('path');
+
+// Configuración de Multer para guardar imágenes de subcategorías
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/subcategorias/'); // Directorio específico
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', subcategoriaController.getAll);
 router.get('/categoria/:idCategoria', subcategoriaController.getByCategoria);
