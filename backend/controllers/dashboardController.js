5bf0eba02c365757509fc8d0efc2fc8f7dfd87f7
AnneVillasante <108015701+AnneVillasante@users.noreply.github.com>
2025-12-03 22:32:53 -0500

    porque si

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -71,7 +71,7 @@ exports.getMetrics = async (req, res) => {
             FROM pedido p
             ${whereClause.length > 0 ? `${whereClause} AND` : 'WHERE'} (p.estado = "pagado" OR p.estado = "entregado")
             GROUP BY ${groupBy}
-            ORDER BY p.fecha ASC
+            ORDER BY MIN(p.fecha) ASC  <-- CAMBIO AQUÍ (Agregamos MIN)
         `;
         const [chartDataRes] = await db.query(chartQuery, params);
 
